621bc7f44870924b7a04857cc416bb6bbb8b4330
zenaldiry <zenaldiry7@gmail.com>
2025-05-28 18:35:36 -0400

    added dist

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static('dist'));
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method);
   console.log('Path:  ', request.path);
